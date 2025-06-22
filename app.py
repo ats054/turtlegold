c4baf8f92f333006ca9117c223e65160b5e32277
ats054 <avshats1@gmail.com>
2025-06-23 02:26:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from logic import check_signals
-import threading  # נדרש כדי להריץ את הבוט במקביל
-import bot  # נניח שהקובץ שלך נקרא bot.py
 
 app = Flask(__name__)
 
@@ -10,11 +8,5 @@ def ping():
     result = check_signals()
     return result if result else "✅ נבדק - אין איתות כרגע"
 
-def run_bot():
-    bot.run_bot()  # פונקציה שניצור מייד בתוך bot.py
-
 if __name__ == "__main__":
-    # הפעלת הבוט בתוך Thread נפרד
-    threading.Thread(target=run_bot).start()
-    # הפעלת האפליקציה
     app.run(host='0.0.0.0', port=10000)
